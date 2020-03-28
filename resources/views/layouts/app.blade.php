d48130cc1d0b5995a3453ec7583148af9ab29bd9
Brieuc Fresnel <fresnel.brieuc@gmail.com>
2020-03-28 01:31:18 +0100

    add responsive searchbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -67,71 +67,73 @@
     </div>
 </nav>
 
-<div class="search-bar">
-    <div class="search-bar__content">
-        <input type="text" class="search-bar__input" name="search-bar__input" placeholder="Search for artists, tracks..."/>
-        <img class="icon" src="{{asset('icons/search-black.png')}}" alt="search"/>
-    </div>
-</div>
+<main>
+    <div class="search-bar">
+        <div class="search-bar__content">
+            <input type="text" class="search-bar__input" name="search-bar__input" placeholder="Search for artists, tracks..."/>
+            <img class="icon" src="{{asset('icons/search-black.png')}}" alt="search"/>
+        </div>
 
-<div class="user-nav user-nav--desktop">
-    <img class="icon" src="{{ asset('icons/icon_user.svg') }}"/>
-    @guest
-        <div class="user-nav__navigation">
-            <li><a class="user-nav__link" href="{{ route('login') }}">Log In</a></li>
-            <li><a class="user-nav__link" href="{{ route('register') }}">Register</a></li>
-        </div>
-    @else
-        <div class="user-nav__navigation">
-            <li><a class="user-nav__link" href="{{ route('login') }}">Profile</a></li>
-            <li> {{/* POST .logout-form on click */}}
-                <a class="user-nav__link" href="{{ route('logout') }}"onclick="
-                        event.preventDefault();
-                        document.getElementById('logout-form').submit();">
-                        Sign Out
-                </a>
-            </li>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                {{ csrf_field() }}
-            </form>
-        </div>
-    @endguest
-</div>
-
-<div id="main" class="container">
-    @yield('content')
-</div>
-
-<div class="bot-bar bot-bar--mobile">
-    <div class="bot-bar__player">
-        <img class="bot-bar__control-btn" src="" alt="previous"/>
-        <img class="bot-bar__control-btn" src="" alt="play/pause"/>
-        <img class="bot-bar__control-btn" src="" alt="next"/>
-    </div>
-    <div class="bot-bar__menu">
-        <div class="bot-bar__link">
-            <img class="bot-bar__menu-icon" alt="home"/>
-            <span>Home</span>
-        </div>
-        <div class="bot-bar__link">
-            <img class="bot-bar__menu-icon" alt="home"/>
-            <span>Library</span>
-        </div>
-        <div class="bot-bar__link">
-            <img class="bot-bar__menu-icon" alt="home"/>
-            <span>Search</span>
+        <div class="user-nav user-nav--desktop">
+            <img class="user-nav__icon" src="{{ asset('icons/icon_user_desktop.svg') }}"/>
+            @guest
+                <div class="user-nav__navigation">
+                    <li><a class="user-nav__link" href="{{ route('login') }}">Log In</a></li>
+                    <li><a class="user-nav__link" href="{{ route('register') }}">Register</a></li>
+                </div>
+            @else
+                <div class="user-nav__navigation">
+                    <li><a class="user-nav__link" href="{{ route('login') }}">Profile</a></li>
+                    <li> {{/* POST .logout-form on click */}}
+                        <a class="user-nav__link" href="{{ route('logout') }}"onclick="
+                                event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                                Sign Out
+                        </a>
+                    </li>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
+                </div>
+            @endguest
         </div>
     </div>
-</div>
-
-<div class="bot-bar bot-bar--desktop">
-    <div class="bot-bar__track-name">Listening to Unnamed by Various Artist</div>
-    <div class="bot-bar__player">
-        <img class="bot-bar__control-btn" src="" alt="previous"/>
-        <img class="bot-bar__control-btn" src="" alt="play/pause"/>
-        <img class="bot-bar__control-btn" src="" alt="next"/>
+    
+    <div id="main" class="container">
+        @yield('content')
     </div>
-</div>
+    
+    <div class="bot-bar bot-bar--mobile">
+        <div class="bot-bar__player">
+            <img class="bot-bar__control-btn" src="" alt="previous"/>
+            <img class="bot-bar__control-btn" src="" alt="play/pause"/>
+            <img class="bot-bar__control-btn" src="" alt="next"/>
+        </div>
+        <div class="bot-bar__menu">
+            <div class="bot-bar__link">
+                <img class="bot-bar__menu-icon" alt="home"/>
+                <span>Home</span>
+            </div>
+            <div class="bot-bar__link">
+                <img class="bot-bar__menu-icon" alt="home"/>
+                <span>Library</span>
+            </div>
+            <div class="bot-bar__link">
+                <img class="bot-bar__menu-icon" alt="home"/>
+                <span>Search</span>
+            </div>
+        </div>
+    </div>
+    
+    <div class="bot-bar bot-bar--desktop">
+        <div class="bot-bar__track-name">Listening to Unnamed by Various Artist</div>
+        <div class="bot-bar__player">
+            <img class="bot-bar__control-btn" src="" alt="previous"/>
+            <img class="bot-bar__control-btn" src="" alt="play/pause"/>
+            <img class="bot-bar__control-btn" src="" alt="next"/>
+        </div>
+    </div>
+</main>
 
 <!-- Scripts -->
 <script src="{{ asset('js/jquery.js') }}"></script>
